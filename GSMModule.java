425ddde34ea3d8717866054583a10574232f163e
Milady-velasquez <ken.velasquezmorales@shoreline.edu>
2026-05-02 14:11:51 -0700

    GSMModule
    simulates sending SMS alerts if there is issues detection.

---
+++ GSMModule.java
@@ -0,0 +1,7 @@
+public class GSMModule {
+
+    public static void sendSMS(String number, String message) {
+        System.out.println("[GSM] Sending SMS to " + number);
+        System.out.println("[GSM] MESSAGE: " + message);
+    }
+}
